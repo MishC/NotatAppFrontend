d013054e1e576d65e4c0cbe7f7f30929e01b5a26
MishC <michaela.chovancova@bergen.kommune.no>
2025-09-02 01:33:16 +0200

    new color set

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,13 +1,17 @@
 // utils/colors.js
 export const colors = [
   "bg-red-100",
-  "bg-blue-100",
-  "bg-green-100",
+  "bg-orange-100",
+  "bg-amber-100",
   "bg-yellow-100",
+  "bg-green-100",
+  "bg-teal-100",
+  "bg-blue-100",
+  "bg-sky-100",
+  "bg-indigo-100",
   "bg-purple-100",
   "bg-pink-100",
-  "bg-orange-100",
-  "bg-teal-100",
+  "bg-rose-100",
 ];
 
 export function getRandomColor() {
