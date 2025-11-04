f8bf70ad296887ba30a211ad2fc4ebc88acbaf49
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-04 14:51:06 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import NoteApp from "./components/NoteApp";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {  Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App mb-[250px]">
-      <Router>
         <Routes>
           <Route path="/" element={<NoteApp />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </Router>
     </div>
   );
 }
