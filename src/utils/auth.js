9285063daeb1bbf22184f1fad90fff6b8d99c3c6
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-17 15:02:54 +0100

    register user

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,17 @@
 const API_LOGIN = `${window.location.origin}/api/auth/login`;
 const API_OTC = `${window.location.origin}/api/auth/verify-2fa`;
+const API_NEW=`/api/auth/register`;
 
+export async function register(email, password, phoneNumber) {
+  const r = await fetch(API_NEW, {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    credentials: "include",
+    body: JSON.stringify({ email, password, phoneNumber })
+  });
+  if (!r.ok) throw new Error("Subscription failed");
+  return r.json();
+}
 
 export async function loginStart(email, password, channel) {
   const r = await fetch(API_LOGIN, {
