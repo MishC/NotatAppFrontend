3d99877b1eca341b9b768b4e1ab3a791ad7d972a
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-22 13:52:26 +0100

    deleted emptz row

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from "./store/store";
 
-
 import './index.css'
 import App from './App.jsx'
 
