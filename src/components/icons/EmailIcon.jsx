5150a04da947896bca0fa7ef6492c452954c55aa
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-26 12:44:55 +0100

    new svg icons

---
+++ src/components/icons/EmailIcon.jsx
@@ -0,0 +1,23 @@
+export default function EmailIcon({ className = "w-[22px] h-[21px] text-slate-300" }) {
+  return (
+    <svg
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M20 19.72H1C1.15833 16.9016 3.28 11.265 10.5 11.265C17.72 11.265 20 16.9016 20 19.72Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M14.535 5.80251C14.535 7.99799 12.7353 9.79001 10.5 9.79001C8.26477 9.79001 6.46503 7.99799 6.46503 5.80251C6.46503 3.60704 8.26477 1.81501 10.5 1.81501C12.7353 1.81501 14.535 3.60704 14.535 5.80251Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/EyeIcon.jsx
@@ -0,0 +1,58 @@
+export default function EyeIcon({
+  isOpen = false,
+  onClick = () => {},
+  className = "w-[21px] h-[21px] text-slate-300 cursor-pointer",
+}) {
+  return (
+    <div onClick={onClick} className="flex items-center">
+      {isOpen ? (
+        <svg
+          width="21"
+          height="21"
+          viewBox="0 0 21 21"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={className}
+        >
+          <circle cx="10.5" cy="10.3" r="3.5" fill="currentColor"></circle>
+          <path
+            d="M10.5 15.55C7.28 15.55 3.95 13.87 1.29 10.3 
+               C3.95 6.73 7.28 5.05 10.5 5.05
+               C13.72 5.05 17.05 6.73 19.71 10.3
+               C17.05 13.87 13.72 15.55 10.5 15.55Z"
+            stroke="currentColor"
+            strokeWidth="1.5"
+          />
+        </svg>
+      ) : (
+        <svg
+          width="21"
+          height="21"
+          viewBox="0 0 21 21"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={className}
+        >
+          <circle cx="10.5" cy="10.3" r="3.5" fill="currentColor"></circle>
+          <path
+            d="M10.5 15.55C7.28 15.55 3.95 13.87 1.29 10.3 
+               C3.95 6.73 7.28 5.05 10.5 5.05
+               C13.72 5.05 17.05 6.73 19.71 10.3
+               C17.05 13.87 13.72 15.55 10.5 15.55Z"
+            stroke="currentColor"
+            strokeWidth="1.5"
+          />
+          <line
+            x1="2"
+            y1="18.5"
+            x2="19"
+            y2="2"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+        </svg>
+      )}
+    </div>
+  );
+}
