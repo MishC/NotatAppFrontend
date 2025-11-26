9dbae89ab35b23ada3c4c0d0b58dde23e6227321
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-26 12:50:25 +0100

    phoneIcon

---
+++ src/components/icons/PhoneIcon.jsx
@@ -0,0 +1,22 @@
+export default function PhoneIcon({
+  className = "w-[21px] h-[21px]",
+}) {
+  return (
+    <svg
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M6.5 2.2L4.9 3.8C4.3 4.4 4 5.1 4 5.8C4 10.8 10.2 17 15.2 17C15.9 17 16.6 16.7 17.2 16.1L18.8 14.5C19.3 14 19.3 13.1 18.7 12.5L15.7 9.5C15.2 9 14.3 8.9 13.7 9.4L12.7 10.2C12.2 10.6 11.5 10.6 11 10.1L10 9.2C9.5 8.7 9.4 7.9 9.9 7.3L10.6 6.5C11.1 6 11 5.1 10.5 4.6L7.5 1.6C6.9 1 6 1 5.4 1.6L6.5 2.2Z"
+        stroke="currentColor"
+        strokeWidth="1.53"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
