546661823eae3f81a980e70b68d5a3ef6adeb212
MishC <michaela.chovancova@bergen.kommune.no>
2025-12-11 14:48:47 +0100

    Add To Calender Button

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -199,6 +199,19 @@ export default function NoteApp() {
       </button>
     );
   })}
+  <button key="calender"       className={[
+        "w-full text-left px-6 py-4 text-2xl font-semibold rounded-lg transition-transform duration-200 ease-out will-change-transform relative cursor-pointer",
+        isActive
+        ? "bg-yellow-100/60 text-slate-900 translate-x-1"
+        : "text-slate-700 hover:text-orange-600 hover:bg-white/40 -translate-x-0.5 hover:translate-x-3",
+      ].join(" ")}
+      >
+      <span
+        className={["absolute left-0 top-0.1 h-[70%] w-1", isActive ? "bg-orange-500" : "bg-transparent"].join(" ")}
+        aria-hidden="true"
+      />
+     Add to Calender
+      </button>
 </div>
 
 
