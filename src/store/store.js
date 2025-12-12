c668c4e60db1086dbea94b28a8da4ff4c2961c40
MishC <michaela.chovancova@bergen.kommune.no>
2025-12-12 23:33:29 +0100

    added action hydrateAuth at the beginning

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../reducers/authSlice";
+import authReducer, { hydrateAuth } from "../reducers/authSlice";
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,  
   },
 });
+store.dispatch(hydrateAuth()); 
