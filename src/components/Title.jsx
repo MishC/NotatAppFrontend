49e5b924ebf3104e15edaaa16cfdd17cacc2c19d
MishC <michaela.chovancova@bergen.kommune.no>
2026-01-21 13:13:09 +0100

    argument name:string

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 
-export default function Title() {
+export default function Title(name) {
   return (
     <div className="Title">
         
-      
+      {name}
     </div>
   )
 }
