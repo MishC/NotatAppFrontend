d90b8e61082016ca0d1fdf4d21505d3505097577
MishC <michaela.chovancova@bergen.kommune.no>
2026-02-12 08:50:52 +0100

    deleted use from import

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React,{ use, useEffect, useMemo, useState } from "react";
+import React,{ useEffect, useMemo, useState } from "react";
 import Plus from "./icons/Plus"; 
 import FolderSettingsModal from "./modals/FolderSettingsModal"; 
 function Sidebar({
