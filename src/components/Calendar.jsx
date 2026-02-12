4520b6c19966e3b85bde4e684fae0483580ea2a2
MishC <michaela.chovancova@bergen.kommune.no>
2026-02-12 21:33:17 +0100

    update

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,31 +1,159 @@
 import { useSelector } from "react-redux";
-import {useState} from "react";
+import React, { useState } from "react";
+import NavigationBar from "./NavigationBar";
+import { formatDate } from '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { INITIAL_EVENTS, createEventId } from "../helpers/event-utils.js"
+
 export default function Calendar() {
   const [error, setError] = useState(null);
   const [msg, setMsg] = useState(null);
-    const user = useSelector((s) => s.auth.user);
+  const [weekendsVisible, setWeekendsVisible] = useState(true)
+  const [currentEvents, setCurrentEvents] = useState([])
+
+  function handleWeekendsToggle() {
+    setWeekendsVisible(!weekendsVisible)
+  }
+
+  function handleDateSelect(selectInfo) {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
+    }
+  }
+
+  function handleEventClick(clickInfo) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove()
+    }
+  }
+
+  function handleEvents(events) {
+    setCurrentEvents(events)
+  }
+  const user = useSelector((s) => s.auth.user);
   const guest = useSelector((s) => s.auth.guest);
+
   return (
-     <div className="w-full m-0 p-0">
-          <div className="sticky top-0 z-10 bg-white/70 backdrop-blur">
-            <NavigationBar
-              userName={user?.name}
-              isNavItemVisble={true}
-              isEmailVisible={false}
-            />
-          </div>
-    
-          <div className={error || msg ? "p-5 rounded-xl" : "p-2"}>
-            {error ? (
-              <div className="text-red-700 p-4 bg-red-50 rounded-xl border border-red-200">{error}</div>
-            ) : msg ? (
-              <div className="text-emerald-700 p-4 bg-emerald-50 rounded-xl border border-emerald-200">{msg}</div>
-            ) : (
-              <div>&nbsp;</div>
-            )}
-          </div>
-          {"Copy and paste from official documentation"}
+    <div className="w-full m-0 p-0">
+      <div className="sticky top-0 z-10 bg-white/70 backdrop-blur">
+        <NavigationBar
+          userName={user?.name}
+          isNavItemVisble={true}
+          isEmailVisible={false}
+        />
+      </div>
+
+      <div className={error || msg ? "p-5 rounded-xl" : "p-5"}>
+        {error ? (
+          <div className="text-red-700 p-4 bg-red-50 rounded-xl border border-red-200">{error}</div>
+        ) : msg ? (
+          <div className="text-emerald-700 p-4 bg-emerald-50 rounded-xl border border-emerald-200">{msg}</div>
+        ) : (
+          <div>&nbsp;</div>
+        )}
+      </div>
+
+
+
+      <div className='demo-app'>
+        <SidebarCalendar
+          weekendsVisible={weekendsVisible}
+          handleWeekendsToggle={handleWeekendsToggle}
+          currentEvents={currentEvents}
+        />
+        <div className='demo-app-main'>
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            initialView='dayGridMonth'
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={weekendsVisible}
+            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            select={handleDateSelect}
+            eventContent={renderEventContent} // custom render function
+            eventClick={handleEventClick}
+            eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+          /* you can update a remote database when these fire:
+          eventAdd={function(){}}
+          eventChange={function(){}}
+          eventRemove={function(){}}
+          */
+          />
+        </div>
+      </div>
+
     </div>
-    
-  );
+  )
 }
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
+}
+
+function SidebarCalendar({ weekendsVisible, handleWeekendsToggle, currentEvents }) {
+  return (
+    <div className='demo-app-sidebar'>
+      <div className='demo-app-sidebar-section'>
+        <h2>Instructions</h2>
+        <ul>
+          <li>Select dates and you will be prompted to create a new event</li>
+          <li>Drag, drop, and resize events</li>
+          <li>Click an event to delete it</li>
+        </ul>
+      </div>
+      <div className='demo-app-sidebar-section'>
+        <label>
+          <input
+            type='checkbox'
+            checked={weekendsVisible}
+            onChange={handleWeekendsToggle}
+          ></input>
+          toggle weekends
+        </label>
+      </div>
+      <div className='demo-app-sidebar-section'>
+        <h2>All Events ({currentEvents.length})</h2>
+        <ul>
+          {currentEvents.map((event) => (
+            <SidebarEvent key={event.id} event={event} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+function SidebarEvent({ event }) {
+  return (
+    <li key={event.id}>
+      <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+      <i>{event.title}</i>
+    </li>
+  )
+}
+
