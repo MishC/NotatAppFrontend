06b99c6f316b23287274e45db39f070c330b5a4b
MishC <michaela.chovancova@bergen.kommune.no>
2026-06-18 15:02:26 +0200

    date input

--- src/components/DateComponent.jsx
+++ src/components/DateComponent.jsx
@@ -10,7 +10,7 @@ export default function DateComponent() {
               <div className=" flex flex-col hidden sm:block text-xs sm:text-xl text-black/70 bg-white/50 px-3 py-2 justify-left">
              <span>{format(today, "EEEE MMMM do")},</span> 
              <span className="text-black/90 font-semibold">&nbsp;{format(today, "HH:mm")}</span>
-             <span className="block text-sm sm:text-base text-black/60 font-semibold">
+             <span className="block text-[24px] text-black/60 font-semibold">
               Week {getIsoWeekNumber(today)}
              </span>
 
